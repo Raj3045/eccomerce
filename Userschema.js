96ada92dfd7370555c00a7d0e1e37c8cd912d907
Raj3045 <97612230+Raj3045@users.noreply.github.com>
2022-01-31 09:06:19 +0530

    Add files via upload

---
+++ Userschema.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose");
+
+
+const userSchema=mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:8
+    },
+    confirmPassword:{
+        type:String,
+        required:true,
+        minLength:8
+
+    }
+    
+});
+const userModel=mongoose.model("userModel",userSchema);
+
+module.exports=userModel;
